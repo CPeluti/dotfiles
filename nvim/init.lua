06bb6b15a9d4866a7a43f4152b788a2da92ce131
Caio Peluti <cpeluti@gmail.com>
2023-09-20 08:25:26 -0300

    changed the dotfiles structure and updated the nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,9 @@
+require "plugins"
+require "keymaps"
+require "whichkey"
+
+
+vim.cmd.colorscheme "catppuccin"
+vim.opt.tabstop=2
+vim.opt.shiftwidth=2
+vim.opt.expandtab=true
---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,4 @@
+local map = vim.keymap.set
+
+--map("n", "<space>", "<Cmd>WhichKey<CR>", {desc = "WhichKey"})
+
